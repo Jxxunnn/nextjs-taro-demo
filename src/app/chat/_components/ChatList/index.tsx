2a065c26f50f88e1b4e0b9d48ad571190d78bf24
Jay Lee <roof2586@naver.com>
2024-11-23 20:50:25 +0900

    feat: 채팅 목록 컴포넌트 구현

---
+++ src/app/chat/_components/ChatList/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Stack } from '@mui/joy';
+import { useEffect, useState } from 'react';
+import ChatMessage from '../ChatMessage';
+
+interface ChatMessage {
+  id: number;
+  message: string;
+  isSender: boolean;
+}
+
+export default function ChatList() {
+  const [chatList, setChatList] = useState<ChatMessage[]>([]);
+
+  useEffect(function sendDelayedWelcomeMessage() {
+    const timer = setTimeout(() => {
+      setChatList((prev) => [
+        {
+          id: 0,
+          message: '오늘은 어떤 주제에 대한 답을 찾고 싶으신가요?',
+          isSender: false,
+        },
+      ]);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <Stack gap="12px">
+      {chatList.map((chat) =>
+        chat.isSender ? (
+          <Box key={chat.id} alignSelf="flex-end" maxWidth="40%" width="fit-content">
+            <ChatMessage isSender message={chat.message} />
+          </Box>
+        ) : (
+          <Box key={chat.id} maxWidth="40%" width="fit-content">
+            <ChatMessage message={chat.message} />
+          </Box>
+        )
+      )}
+    </Stack>
+  );
+}
