702e78f645d43800800cd18c3d44700666d59007
Jay Lee <roof2586@naver.com>
2024-11-23 20:39:29 +0900

    feat: 외부 이미지 src 제거

--- src/app/chat/_components/ChatMessage/index.tsx
+++ src/app/chat/_components/ChatMessage/index.tsx
@@ -1,6 +1,5 @@
 import { Avatar, Stack } from '@mui/joy';
 import ChatBubble from '../ChatBubble';
-
 interface ChatMessageProps {
   isSender?: boolean;
   message: string;
@@ -12,7 +11,7 @@ export default function ChatMessage({ isSender, message }: ChatMessageProps) {
         <ChatBubble isSender message={message} />
       ) : (
         <Stack direction="row" gap="16px">
-          <Avatar size="sm" src="https://source.unsplash.com/random/100x100" />
+          <Avatar size="sm" />
           <ChatBubble message={message} />
         </Stack>
       )}
