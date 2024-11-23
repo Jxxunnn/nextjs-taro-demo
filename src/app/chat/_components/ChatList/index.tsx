08f16d5b82f0d086549f4d2ac5b3af749d52ebc2
Jay Lee <roof2586@naver.com>
2024-11-23 22:05:32 +0900

    feat: 제어 컴포넌트로 전환 및 채팅 메시지 간 간격 조절

--- src/app/chat/_components/ChatList/index.tsx
+++ src/app/chat/_components/ChatList/index.tsx
@@ -1,39 +1,36 @@
 import { Box, Stack } from '@mui/joy';
-import { useEffect, useState } from 'react';
 import ChatMessage from '../ChatMessage';
 
-interface ChatMessage {
+export interface ChatMessage {
   id: number;
   message: string;
   isSender: boolean;
 }
 
-export default function ChatList() {
-  const [chatList, setChatList] = useState<ChatMessage[]>([]);
-
-  useEffect(function sendDelayedWelcomeMessage() {
-    const timer = setTimeout(() => {
-      setChatList((prev) => [
-        {
-          id: 0,
-          message: '오늘은 어떤 주제에 대한 답을 찾고 싶으신가요?',
-          isSender: false,
-        },
-      ]);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+interface ChatListProps {
+  chatList: ChatMessage[];
+}
+export default function ChatList({ chatList }: ChatListProps) {
   return (
-    <Stack gap="12px">
-      {chatList.map((chat) =>
+    <Stack>
+      {chatList.map((chat, index) =>
         chat.isSender ? (
-          <Box key={chat.id} alignSelf="flex-end" maxWidth="40%" width="fit-content">
+          <Box
+            key={chat.id}
+            alignSelf="flex-end"
+            maxWidth="40%"
+            width="fit-content"
+            marginTop={index > 0 && chatList[index - 1].isSender === chat.isSender ? '6px' : '12px'}
+          >
             <ChatMessage isSender message={chat.message} />
           </Box>
         ) : (
-          <Box key={chat.id} maxWidth="40%" width="fit-content">
+          <Box
+            key={chat.id}
+            maxWidth="40%"
+            width="fit-content"
+            marginTop={index > 0 && chatList[index - 1].isSender === chat.isSender ? '6px' : '12px'}
+          >
             <ChatMessage message={chat.message} />
           </Box>
         )
