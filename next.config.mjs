a7eb394a80cf4fff80db63fce3a4768580e1124a
Jay Lee <roof2586@naver.com>
2024-11-24 00:47:23 +0900

    feat: 홈에서 채팅 페이지로 리디렉션 되도록 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/chat',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
