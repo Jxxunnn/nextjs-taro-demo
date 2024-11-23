e489b34c7c091e2689e41b5643b562e904cdfd89
Jay Lee <roof2586@naver.com>
2024-11-23 20:50:57 +0900

    feat: 채팅 페이지 레이아웃 개선

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,13 +1,19 @@
-import theme from '@/lib/mui/theme';
-import { Box } from '@mui/joy';
+import { Box, Sheet } from '@mui/joy';
 import { ReactNode } from 'react';
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
   return (
-    <Box component="section" minHeight="100dvh" display="flex" flexDirection="column">
-      <Box flex={1} width="100%" maxWidth={theme.breakpoints.values.md} marginX="auto">
+    <Sheet
+      component="section"
+      sx={{
+        height: '100dvh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Box height={'100%'} display="flex" flexDirection="column" flex={1}>
         {children}
       </Box>
-    </Box>
+    </Sheet>
   );
 }
